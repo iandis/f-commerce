ff08356b1e7ffe08096fc96dd59772b0efa62fcc
Iandi Santulus <iandisantulusn@gmail.com>
2021-07-31 05:35:24 +0700

    added AppTheme

---
+++ lib/core/constants/app_theme.dart
@@ -0,0 +1,10 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFF0D47A1);
+  static const Color secondaryColor = Color(0xFF1565C0);
+  static const Color accentColor = Color(0xFFFFC107);
+}
