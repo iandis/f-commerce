8d2e72f3de5b60f5fc1c0aa5fdc5027569c398f7
Iandi Santulus <iandisantulusn@gmail.com>
2021-07-31 05:35:03 +0700

    added AppRoutes

---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,8 @@
+
+class AppRoutes {
+  static const String home = '/';
+  static const String details = '/details';
+  static const String cart = '/cart';
+  static const String confirmation = '/confirmation';
+  static const String success = '/success';
+}
