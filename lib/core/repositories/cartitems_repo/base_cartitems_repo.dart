80cbd7d4d5b4bc95836457d4583537e08dfa2f20
Iandi Santulus <iandisantulusn@gmail.com>
2021-07-31 05:44:07 +0700

    added getCartItemCount and modified StreamController to BehaviorSubject

--- lib/core/repositories/cartitems_repo/base_cartitems_repo.dart
+++ lib/core/repositories/cartitems_repo/base_cartitems_repo.dart
@@ -1,16 +1,19 @@
 import 'dart:async';
 
+import 'package:rxdart/subjects.dart';
 import '/core/models/cart/cart_item.dart';
 
 abstract class BaseCartItemsRepository {
-  StreamController<int> get cartItemCountController;
-  Future<int> getCartItemCount();
+  BehaviorSubject<int> get cartItemCountController;
+  Future<int> getCartItemsCount();
+  Future<int> getCartItemCount(int id);
   Future<List<CartItem>> getCartItemList();
   Future<void> insertCartItem(CartItem cartItem);
   Future<void> updateCartItemAmount({
     required int id,
     required int newAmount,
   });
+  Future<void> deleteCartItems(List<int> ids);
   Future<void> deleteCartItem(int id);
   Future<bool> isInCartItem(int id);
   Future<void> close();
