9f907d9190e98992348237442a6ab303911f2855
Iandi Santulus <iandisantulusn@gmail.com>
2021-07-31 05:36:50 +0700

    fixed bug on CartItem having no id

--- lib/core/models/cart/cart_item.dart
+++ lib/core/models/cart/cart_item.dart
@@ -2,20 +2,35 @@ import '../product/product.dart';
 
 class CartItem {
   const CartItem({
+    required this.id,
     required this.product,
     required this.amount,
   });
 
+  final int id;
   final Product product;
   final int amount;
 
   double get totalPrice => amount * product.price;
 
+  factory CartItem.fromProduct({
+    required Product product,
+    required int amount,
+  }) {
+    return CartItem(
+      id: product.id,
+      product: product,
+      amount: amount,
+    );
+  }
+
   CartItem copyWith({
+    int? id,
     Product? product,
     int? amount,
   }) {
     return CartItem(
+      id: id ?? this.id,
       product: product ?? this.product,
       amount: amount ?? this.amount,
     );
@@ -23,6 +38,7 @@ class CartItem {
 
   Map<String, dynamic> toMap({bool toLocalDb = false}) {
     return {
+      'id': id,
       'product': toLocalDb ? product.toJson() : product.toMap(),
       'amount': amount,
     };
@@ -32,28 +48,26 @@ class CartItem {
     Map<String, dynamic> map, {
     bool fromLocalDb = false,
   }) {
-    final product = fromLocalDb
-        ? Product.fromJson(map['product'] as String)
-        : Product.fromMap(map['product'] as Map<String, dynamic>);
-        
+    final product =
+        fromLocalDb ? Product.fromJson(map['product'] as String) : Product.fromMap(map['product'] as Map<String, dynamic>);
+
     return CartItem(
+      id: map['id'] as int,
       product: product,
       amount: map['amount'] as int,
     );
   }
 
   @override
-  String toString() => 'CartItem(product: $product, amount: $amount)';
+  String toString() => 'CartItem(id: $id, product: $product, amount: $amount)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is CartItem &&
-      other.product == product &&
-      other.amount == amount;
+    return other is CartItem && other.id == id && other.product == product && other.amount == amount;
   }
 
   @override
-  int get hashCode => product.hashCode ^ amount.hashCode;
+  int get hashCode => id.hashCode ^ product.hashCode ^ amount.hashCode;
 }
