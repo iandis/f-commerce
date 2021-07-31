dd6e082ca1b1b854645116fd8972b1c58cd48a77
Iandi Santulus <iandisantulusn@gmail.com>
2021-07-31 11:19:21 +0700

    added SuccessScreen

---
+++ lib/views/success/success_screen.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+import '/core/constants/app_routes.dart';
+import '/core/constants/app_theme.dart';
+import '/core/helpers/formatters.dart';
+import '/core/models/cart/checkout_items.dart';
+import '/core/services/navigation_service/base_navigation_service.dart';
+import '/views/_widgets/cart_item_tile.dart';
+
+class SuccessScreen extends StatefulWidget {
+  final CheckoutItems checkoutItems;
+  const SuccessScreen({
+    Key? key,
+    required this.checkoutItems,
+  }) : super(key: key);
+
+  @override
+  _SuccessScreenState createState() => _SuccessScreenState();
+}
+
+class _SuccessScreenState extends State<SuccessScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        GetIt.I<BaseNavigationService>().backUntilNamed(AppRoutes.home);
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Success'),
+          brightness: Brightness.dark,
+          centerTitle: true,
+        ),
+        body: Container(
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          margin: const EdgeInsets.only(bottom: 56),
+          child: confirmationSummary,
+        ),
+        bottomSheet: totalPriceCell,
+      ),
+    );
+  }
+
+  Widget get userAddressText {
+    final address = Text(
+      widget.checkoutItems.destination,
+      maxLines: 3,
+      textAlign: TextAlign.justify,
+    );
+    return Row(
+      children: [
+        const Icon(
+          Icons.location_on,
+          color: Colors.grey,
+        ),
+        const SizedBox(width: 5),
+        SizedBox(
+          height: 50,
+          width: MediaQuery.of(context).size.width - 50,
+          child: address,
+        ),
+      ],
+    );
+  }
+
+  Widget get invoiceIdText {
+    return Row(
+      children: [
+        const Icon(
+          Icons.receipt_sharp,
+          color: Colors.grey,
+        ),
+        const SizedBox(width: 5),
+        Text(widget.checkoutItems.invoiceId),
+      ],
+    );
+  }
+
+  Widget get confirmationSummary {
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              SizedBox(height: 20),
+              Text(
+                'Summary',
+                style: TextStyle(
+                  color: AppTheme.secondaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 21,
+                ),
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              invoiceIdText,
+              const Divider(),
+              userAddressText,
+              const Divider(),
+            ],
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (_, index) {
+              return CartItemTile(
+                cartItem: widget.checkoutItems.cartItems[index],
+              );
+            },
+            childCount: widget.checkoutItems.cartItems.length,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget get totalPriceCell {
+    return SizedBox(
+      height: 56,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Total',
+            style: TextStyle(color: Colors.grey),
+          ),
+          Text(
+            Formatters.formatPrice(widget.checkoutItems.totalCheckoutPrice),
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
