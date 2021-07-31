45de28eeb982edbb912f9a0f4c5dc3b6cdbc5241
Iandi Santulus <iandisantulusn@gmail.com>
2021-07-31 11:19:59 +0700

    added LocationService to dependency injection

--- lib/singletons.dart
+++ lib/singletons.dart
@@ -1,21 +1,22 @@
-
 import 'package:get_it/get_it.dart';
 
-import '/core/repositories/cartitems_repo/base_cartitems_repo.dart';
-import '/core/repositories/cartitems_repo/cartitems_repo.dart';
-
-import '/core/repositories/products_repo/base_products_repo.dart';
-import '/core/repositories/products_repo/products_repo.dart';
-
-import '/core/services/http_service/base_http_service.dart';
-import '/core/services/http_service/http_service.dart';
-
-import '/core/services/localdb_service/base_localdb_service.dart';
-import '/core/services/localdb_service/localdb_service.dart';
+import 'core/repositories/cartitems_repo/base_cartitems_repo.dart';
+import 'core/repositories/cartitems_repo/cartitems_repo.dart';
+import 'core/repositories/products_repo/base_products_repo.dart';
+import 'core/repositories/products_repo/products_repo.dart';
+import 'core/services/http_service/base_http_service.dart';
+import 'core/services/http_service/http_service.dart';
+import 'core/services/localdb_service/base_localdb_service.dart';
+import 'core/services/localdb_service/localdb_service.dart';
+import 'core/services/location_service/base_location_service.dart';
+import 'core/services/location_service/location_service.dart';
+import 'core/services/navigation_service/base_navigation_service.dart';
+import 'core/services/navigation_service/navigation_service.dart';
 
 void initSingletons() {
-  GetIt.I.registerSingleton<BaseHttpService>(HTTPService());
+  GetIt.I.registerSingleton<BaseHttpService>(HttpService());
   GetIt.I.registerSingleton<BaseLocalDbService>(LocalDbService()..initDb());
+  GetIt.I.registerSingleton<BaseNavigationService>(NavigationService());
   GetIt.I.registerSingleton<BaseProductsRepository>(
     ProductsRepository(
       httpService: GetIt.I<BaseHttpService>(),
@@ -26,4 +27,6 @@ void initSingletons() {
       localDbService: GetIt.I<BaseLocalDbService>(),
     ),
   );
+
+  GetIt.I.registerLazySingleton<BaseLocationService>(() => LocationService());
 }
