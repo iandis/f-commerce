1b116a3d08bc3e7e11e1b599ffed6f0429b40f55
Iandi Santulus <iandisantulusn@gmail.com>
2021-07-31 11:12:08 +0700

    modified AppTheme

--- lib/core/constants/app_theme.dart
+++ lib/core/constants/app_theme.dart
@@ -7,4 +7,5 @@ class AppTheme {
   static const Color primaryColor = Color(0xFF0D47A1);
   static const Color secondaryColor = Color(0xFF1565C0);
   static const Color accentColor = Color(0xFFFFC107);
+  static const Color green = Color(0xFF00C853);
 }
