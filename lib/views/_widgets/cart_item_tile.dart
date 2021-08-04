df8232cf7ac01de4fbeb6b7bd9d4f32459083454
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-04 20:44:34 +0700

    modified CartItemTile, splitted ListTile children to reduce widget nesting

--- lib/views/_widgets/cart_item_tile.dart
+++ lib/views/_widgets/cart_item_tile.dart
@@ -13,10 +13,6 @@ import '/views/_widgets/number_spinner.dart';
 /// and if [onRemove] is null,
 /// this will show item amount instead.
 class CartItemTile extends StatelessWidget {
-  final VoidCallback? onIncrement;
-  final VoidCallback? onDecrement;
-  final VoidCallback? onRemove;
-  final EdgeInsetsGeometry? contentPadding;
   const CartItemTile({
     Key? key,
     required this.cartItem,
@@ -27,57 +23,91 @@ class CartItemTile extends StatelessWidget {
   }) : super(key: key);
 
   final CartItem cartItem;
+  final VoidCallback? onIncrement;
+  final VoidCallback? onDecrement;
+  final VoidCallback? onRemove;
+  final EdgeInsetsGeometry? contentPadding;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       contentPadding: contentPadding ?? EdgeInsets.zero,
-      minLeadingWidth: kMinInteractiveDimension,
-      leading: Container(
-        clipBehavior: Clip.hardEdge,
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-        child: CachedNetworkImage(imageUrl: cartItem.product.image),
-      ),
-      title: Text(cartItem.product.name),
+      leading: itemImage,
+      title: itemName,
       subtitle: itemCountCell,
-      trailing: onRemove != null
-          ? IconButton(
-              onPressed: onRemove,
-              color: Colors.redAccent,
-              icon: const Icon(Icons.delete),
-            )
-          : Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 7.5,
-                vertical: 2.5,
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                color: AppTheme.accentColor,
-              ),
-              child: Text('${cartItem.amount}'),
-            ),
+      trailing: itemTrailing,
+    );
+  }
+
+  Widget get itemImage {
+    return Container(
+      width: kMinInteractiveDimension,
+      height: kMinInteractiveDimension,
+      clipBehavior: Clip.hardEdge,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: CachedNetworkImage(
+        imageUrl: cartItem.product.image,
+      ),
+    );
+  }
+
+  Widget get itemName {
+    return Text(
+      cartItem.product.name,
+      maxLines: 1,
+      style: const TextStyle(fontWeight: FontWeight.bold),
     );
   }
 
   Widget get itemCountCell {
+    final itemPrice = Text(Formatters.formatPrice(cartItem.totalPrice));
+
     final Widget boxChild;
+
     if (onDecrement != null && onIncrement != null) {
-      boxChild = NumberSpinner(
-        value: cartItem.amount,
-        lowerLimit: 1,
-        onIncrement: onIncrement,
-        onDecrement: onDecrement,
+      boxChild = Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(flex: 3, child: itemPrice),
+          Flexible(
+            flex: 2,
+            child: NumberSpinner(
+              value: cartItem.amount,
+              lowerLimit: 1,
+              onIncrement: onIncrement,
+              onDecrement: onDecrement,
+            ),
+          ),
+        ],
       );
     } else {
-      boxChild = Text('Total: ${Formatters.formatPrice(cartItem.totalPrice)}');
+      boxChild = itemPrice;
     }
-    return FractionallySizedBox(
-      alignment: Alignment.centerLeft,
-      widthFactor: 0.5,
-      child: boxChild,
+    return boxChild;
+  }
+
+  Widget get itemTrailing {
+    if (onRemove != null) {
+      return IconButton(
+        onPressed: onRemove,
+        color: Colors.redAccent,
+        icon: const Icon(Icons.delete),
+      );
+    }
+
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 7.5,
+        vertical: 2.5,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: AppTheme.accentColor,
+      ),
+      child: Text('${cartItem.amount}'),
     );
   }
 }
